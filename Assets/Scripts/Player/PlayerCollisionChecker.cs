3699c68d4fbcbc84967f3ed983876b4e15714945
UnstableFactor <787121798@qq.com>
2020-01-23 23:00:58 +0800

    feat(*): Initial game Project and build basic player
    Initial Game Project:
    - Add PlayerController,now player can move,jump,and climb but they are all incomplete
    - Add CMCamera

---
+++ Assets/Scripts/Player/PlayerCollisionChecker.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionChecker : MonoBehaviour
+{
+    private PlayerController pc;
+
+    [Header("环境检测设置")]
+    public float rayCastLength = 0.2f;
+    public LayerMask environment;
+
+    [Header("环境状态检测")]
+    public bool onGround;
+    public bool canClimb;
+
+    [Header("检测设置")]
+    public Vector2 offsetGround;
+    public Vector2 offsetClimb_Bottom;
+    public Vector2 offsetClimb_Top;
+    public Vector2 offsetClimb_Down;
+
+    void Start()
+    {
+        pc = GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        CheckOnGround();
+        CheckCanClimb();
+    }
+
+    void CheckOnGround() {
+        RaycastHit2D hit = Raycast(offsetGround, Vector2.down, rayCastLength, environment);
+        if (hit) {
+            onGround = true;
+        }
+    }
+
+    void CheckCanClimb() {
+        RaycastHit2D hit_Bottom = Raycast(offsetClimb_Bottom, Vector2.right * pc.direction, rayCastLength * 3, environment);
+        RaycastHit2D hit_Top = Raycast(offsetClimb_Top, Vector2.right *pc.direction, rayCastLength * 3, environment);
+        RaycastHit2D hit_Down = Raycast(offsetClimb_Down, Vector2.down, rayCastLength * 3, environment);
+
+        if (hit_Bottom && hit_Down && !hit_Top)
+        {
+            canClimb = true;
+            // 修正人物位置
+            pc.FixPlayerClimbPosition(new Vector2(hit_Bottom.distance, hit_Down.distance));
+        }
+    }
+    
+    /// <summary>
+    /// 重载2D射线 将射线起始点定位于物体中心
+    /// </summary>
+    /// <param name="_offset"></param>
+    /// <param name="_direction"></param>
+    /// <param name="_length"></param>
+    /// <param name="_layer"></param>
+    /// <returns></returns>
+    RaycastHit2D Raycast(Vector2 _offset, Vector2 _direction, float _length, LayerMask _layer) {
+        // 左右方向修正
+        Vector2 pos = transform.position;
+        Vector2 offset = _offset;
+        offset.x *= pc.direction;
+
+        RaycastHit2D hit = Physics2D.Raycast(pos + offset, _direction, _length, _layer);
+
+        Color color = hit ? Color.red : Color.green;
+        Debug.DrawRay(pos + offset,_direction * _length, color);
+        return hit;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private SpriteRenderer sr;
+    private Rigidbody2D rb;
+    private PlayerCollisionChecker pcc;
+
+    [Header("人物属性")]
+    public float speed;
+    public float jumpSpeed;
+
+    [Header("人物状态")]
+    public int direction;
+
+    [Header("输入检测")]
+    public float axisX;
+    public float axisY;
+    public float axisRawX;
+    public float axisRawY;
+
+    [Header("行为状态")]
+    public bool jumpPressed;
+    public bool isJumping;
+    public bool isClimbing;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        pcc = GetComponent<PlayerCollisionChecker>();
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+        Climb();
+    }
+
+    private void CheckInput() {
+        axisX = Input.GetAxis("Horizontal");
+        axisRawX = Input.GetAxisRaw("Horizontal");
+        axisY = Input.GetAxis("Vertical");
+        axisRawY = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.W) && pcc.onGround) {
+            jumpPressed = true;
+        }
+    }
+
+    /// <summary>
+    /// 角色移动
+    /// </summary>
+    private void Move()
+    {
+        // 仅地面上可移动
+        if (pcc.onGround)
+        {
+            rb.velocity = new Vector2(axisX * speed, rb.velocity.y);
+            switchDirection(axisRawX);
+        }
+    }
+
+    /// <summary>
+    /// 转换人物朝向
+    /// </summary>
+    /// <param name="_dir">0->左 1->右</param>
+    void switchDirection(float _dir) {
+        if (_dir == 1)
+        {
+            direction = 1;
+            sr.flipX = false;
+        }
+        else if(_dir == -1){
+            direction = -1;
+            sr.flipX = true;
+        }
+    }
+
+    /// <summary>
+    /// 角色跳跃
+    /// </summary>
+    private void Jump() {
+        // 起跳
+        if (jumpPressed && pcc.onGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+
+            isJumping = true;
+            jumpPressed = false;
+            pcc.onGround = false;
+        }
+        // 跳跃结束 重置跳跃状态
+        else
+        {
+            isJumping = false;
+        }
+    }
+
+    /// <summary>
+    /// 角色攀爬
+    /// </summary>
+    private void Climb() {
+        if (pcc.canClimb) {
+            rb.bodyType = RigidbodyType2D.Static;
+
+            isClimbing = true;
+
+            // 此处应衔接动画 匹配动画演出点
+        }
+    }
+
+    /// <summary>
+    /// 修正人物攀爬动作起始位置
+    /// </summary>
+    /// <param name="offset">二维向量偏移量 X作为底部射线到碰撞体距离 Y为下侧射线到碰撞体距离</param>
+    public void FixPlayerClimbPosition(Vector2 offset) {
+        Vector2 pos = transform.position;
+        pos.x += offset.x * direction;
+        pos.y -= offset.y;
+
+        transform.position = pos;
+    }
+}
